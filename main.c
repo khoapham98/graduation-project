d1bc03dbb97966f972b61dbae0a3520cfa03023b
khoapham98 <phamanhkhoa9803@gmail.com>
2025-09-23 10:58:30 +0700

    testing ADS1115 APIs successfully

--- main.c
+++ main.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
-#include "ads1115.h"
+#include "inc/ads1115.h"
+
+#define 	FILE_PATH			"/dev/i2c-2"
+#define 	ADS1115_BASE_ADDR	0x48
 
 int main()
 {
-	ads1115_init();
+	int fd = ads1115_init(FILE_PATH, ADS1115_BASE_ADDR);
+	unsigned char reg = 0x01;
+	while (1) 
+	{
+		if (reg > 0x04)
+			reg = 0x01;
+		int tmp = ads1115_get_reg_val(fd, &reg);
+		printf("%d\n", tmp);
+		reg++;
+	}
 
 	return 0;
 }
