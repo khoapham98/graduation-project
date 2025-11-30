1778882957617afe9f44abe241d049b3f21724ce
Khoa Pham <anh.khoa@industry.com.vn>
2025-11-30 18:14:51 +0700

    delete build files

--- src/drivers/log.h
+++
@@ -1,8 +0,0 @@
-#ifndef _LOG_H_
-#define _LOG_H_
-
-#define LOG_INF(fmt, ...) printf("[INFO] " fmt "\n", ##__VA_ARGS__)
-#define LOG_WRN(fmt, ...) printf("[WARN] " fmt "\n", ##__VA_ARGS__)
-#define LOG_ERR(fmt, ...) printf("[ERRO] " fmt "\n", ##__VA_ARGS__)
-
-#endif
