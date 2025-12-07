3ef6d37f6c91e6d0f481b4d3c422b46fc9f92d15
Khoa Pham <phamanhkhoa9803@gmail.com>
2025-12-07 16:39:08 +0700

    update setup function for sim module

--- src/device_setup.c
+++ src/device_setup.c
@@ -13,6 +13,8 @@
 #include "src/dust_sensor/dust_sensor.h"
 #include "src/gps/gps.h"
 #include "sys/json.h"
+#include "src/sim/at_cmd.h"
+#include "src/sim/sim.h"
 
 /* thread array for manage */
 pthread_t thread[MAX_THREADS];
@@ -63,14 +65,23 @@ void* updateGPSTask(void* arg)
 void* send2WebTask(void* arg)
 {
 	while (1) {
+#if GPS_ENABLE
         sem_wait(&gpsDataReadySem);        
         double lat = latitude;
         double lon = longitude;
         sem_post(&gpsDataDoneSem);
+#else
+        double lat = -1;
+        double lon = -1;
+#endif
 
+#if DUST_SENSOR_ENABLE
         sem_wait(&dustDataReadySem);        
         uint16_t pm25 = pm2_5;
         sem_post(&dustDataDoneSem);
+#else
+        uint16_t pm25 = -1;
+#endif
 
         parseAllDataToJson(&json_ring_buf, lat, lon, pm25);
         char web_buf[256] = {0};
@@ -113,9 +124,14 @@ static int setupGPS(void)
     return err;
 }
 
-static int setup4G(void) 
+static int setupSim(void) 
 {
-    int err = pthread_create(&thread[2], NULL, send2WebTask, NULL);
+    int err = sim_init(UART5_FILE_PATH);    
+    if (err != 0)
+        return err;
+
+    simInitialCheck();
+    err = pthread_create(&thread[2], NULL, send2WebTask, NULL);
     if (err != 0) 
         LOG_ERR("pthread_create: %d", err);
 
@@ -126,17 +142,21 @@ int deviceSetup(void)
 {
     ring_buffer_init(&json_ring_buf, json_ring_buf_data, sizeof(json_ring_buf_data));
 
-    int err = 0; 
+    int err = 0;
 
+#if DUST_SENSOR_ENABLE
     err = setupDustSensor();
     if (err != 0)
         LOG_ERR("Failed to setup dust sensor");
+#endif 
 
+#if GPS_ENABLE
     err = setupGPS();
     if (err != 0)
         LOG_ERR("Failed to setup GPS");
+#endif
 
-    err = setup4G();
+    err = setupSim();
     if (err != 0)
         LOG_ERR("Failed to setup 4G module");
 
