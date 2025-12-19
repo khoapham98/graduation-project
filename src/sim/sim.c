69aef9e649fbf96ab3405c21bf9902a6280d5897
Khoa Pham <phamanhkhoa9803@gmail.com>
2025-12-19 11:07:54 +0700

    add sim FSM handler and encapsulate state handlers

--- src/sim/sim.c
+++ src/sim/sim.c
@@ -6,25 +6,39 @@
 #include "sys/log.h"
 #include "sim_cmd.h"
 #include "sim.h"
-#include "transport/mqtt.h"
+#include "fsm/fsm.h"
 
-eSimState preState = STATE_RESET;
-eSimState simState = STATE_RESET;
+static eSimState preState = SIM_STATE_RESET;
 
-void updateSimState(eSimResult res, eSimState backState, eSimState nextState)
+static const char* simStateStr[] = {
+    "SIM_STATE_RESET",
+    "SIM_STATE_AT_SYNC",
+    "SIM_STATE_SIM_READY",
+    "SIM_STATE_NET_READY",
+    "SIM_STATE_PDP_ACTIVE"
+};
+
+static void updateSimState(eSimResult res, eSimState backState, eSimState nextState)
 {
     if (res == FAIL) {
-        simState = preState;
+        setSimState(preState);
         preState = backState;
-    } else if (res == PASS) {
-        preState = simState;
-        simState = nextState;
-    } else {
+    } 
+    else if (res == PASS) {
+        if (getSimState() == SIM_STATE_PDP_ACTIVE) {
+            setFsmLayer(FSM_LAYER_TRANSPORT);
+            return;
+        }
+
+        preState = getSimState();
+        setSimState(nextState);
+    } 
+    else {
         sleep(1);
     }
 }
 
-void simResetStatusHandler(void)
+static void simResetStatusHandler(void)
 {
     while (1) {
         if (simCheckAlive() == PASS)
@@ -32,36 +46,32 @@ void simResetStatusHandler(void)
         sleep(1);
     }
 
-    mqttDisconnectHandler();
-
-    mqttReleaseClientHandler();
-
-    updateSimState(PASS, STATE_RESET, STATE_AT_SYNC);
+    updateSimState(PASS, SIM_STATE_RESET, SIM_STATE_AT_SYNC);
 } 
 
-void atSyncStatusHandler(void)
+static void atSyncStatusHandler(void)
 {
     eSimResult res = PASS;
     if (simCheckAlive() == FAIL || simEchoOff() == FAIL) {
         res = FAIL;
     }
 
-    updateSimState(res, STATE_RESET, STATE_SIM_READY);
+    updateSimState(res, SIM_STATE_RESET, SIM_STATE_SIM_READY);
 }
 
-void simReadyStatusHandler(void)
+static void simReadyStatusHandler(void)
 {
     eSimResult res = simCheckReady();
-    updateSimState(res, STATE_RESET, STATE_NET_READY);
+    updateSimState(res, SIM_STATE_RESET, SIM_STATE_NET_READY);
 }
 
-void netReadyStatusHandler(void)
+static void netReadyStatusHandler(void)
 {
     eSimResult res = simCheckRegEps();
-    updateSimState(res, STATE_AT_SYNC, STATE_PDP_ACTIVE);
+    updateSimState(res, SIM_STATE_AT_SYNC, SIM_STATE_PDP_ACTIVE);
 }
 
-void pdpActiveStatusHandler(void)
+static void pdpActiveStatusHandler(void)
 {
     eSimResult res = simSetPdpContext();
     if (res != PASS)    
@@ -74,5 +84,28 @@ void pdpActiveStatusHandler(void)
     res = simActivatePdp();
 
 end:
-    updateSimState(res, STATE_SIM_READY, STATE_MQTT_START);
+    updateSimState(res, SIM_STATE_SIM_READY, SIM_STATE_PDP_ACTIVE);
+}
+
+void simFsmHandler(eSimState state)
+{
+    LOG_INF("%s", simStateStr[state]);
+    switch (state)
+    {
+    case SIM_STATE_RESET:
+        simResetStatusHandler();
+        break;
+    case SIM_STATE_AT_SYNC:
+        atSyncStatusHandler();
+        break;
+    case SIM_STATE_SIM_READY:
+        simReadyStatusHandler();
+        break;
+    case SIM_STATE_NET_READY:
+        netReadyStatusHandler();
+        break;
+    case SIM_STATE_PDP_ACTIVE:
+        pdpActiveStatusHandler();
+        break;
+    }
 }
--- src/sim/sim.h
+++ src/sim/sim.h
@@ -5,51 +5,13 @@
 #ifndef _SIM_H_
 #define _SIM_H_
 #include "sim_cmd.h"
-
-enum simState {
-    STATE_RESET,
-    STATE_AT_SYNC,
-    STATE_SIM_READY,
-    STATE_NET_READY,
-    STATE_PDP_ACTIVE,        
-    STATE_MQTT_START,
-    STATE_MQTT_ACCQ,
-    STATE_MQTT_CONNECT,
-    STATE_MQTT_READY
-};
-
-typedef enum simState eSimState;
-
-void updateSimState(eSimResult res, eSimState backState, eSimState nextState);
+#include "fsm/fsm.h"
 
 /**
- * @brief Handle SIM reset state.
+ * @brief Handle SIM layer FSM based on current SIM state.
+ * @param state Current SIM state to be processed.
  * @return none.
  */
-void simResetStatusHandler(void);
-
-/**
- * @brief Handle AT synchronization state.
- * @return none.
- */
-void atSyncStatusHandler(void);
-
-/**
- * @brief Handle SIM ready check state.
- * @return none.
- */
-void simReadyStatusHandler(void);
-
-/**
- * @brief Handle network registration state.
- * @return none.
- */
-void netReadyStatusHandler(void);
-
-/**
- * @brief Handle PDP activation state.
- * @return none.
- */
-void pdpActiveStatusHandler(void);
+void simFsmHandler(eSimState state);
 
 #endif
