9e5377844868d35b9e42fcd6e280e94e08ab2cc3
Khoa Pham <phamanhkhoa9803@gmail.com>
2025-12-22 00:54:04 +0700

    refactor: align JSON format with server spec and remove unused functions

--- sys/json.c
+++ sys/json.c
@@ -13,45 +13,15 @@ void getJsonData(ring_buffer_t* rb, char* buf)
     ring_buffer_dequeue_arr(rb, buf, ring_buf_size);
 }
 
-void parseAllDataToJson(ring_buffer_t* rb, double lat, double lon, uint16_t pm2_5)
+void parseAllDataToJson(ring_buffer_t* rb, char* key, int row, int col, uint16_t pm2_5)
 {
     char json_buf[256] = {0};
     snprintf(json_buf, sizeof(json_buf), 
-            "{"         
-            "\"latitude\":%lf,\"longitude\":%lf,"
-            "\"altitude_m\":%d,"
-            "\"sensor_value\":%u"
-            "}", 
-            lat, lon, 50, pm2_5);
+            "{\"%s\":"
+                "{\"%s-%d-%d\":"
+                    "{\"sensor_value\":%d}"
+                "}"
+            "}",
+            key, key, col, row, pm2_5);
     ring_buffer_queue_arr(rb, json_buf, strlen(json_buf));
-}
-
-void parseGpsDataToJson(ring_buffer_t* rb, double lat, double lon)
-{
-	char gps_json_buf[64] = {0};
-	snprintf(gps_json_buf, sizeof(gps_json_buf),
-            "{"
-			"\"latitude\":%lf,\"longitude\":%lf,", lat, lon);
-	ring_buffer_queue_arr(rb, gps_json_buf, strlen(gps_json_buf));
-}
-
-void parseDustDataToJson(ring_buffer_t* rb, uint16_t pm2_5)
-{
-	char dust_json_buf[64] = {0};
-	snprintf(dust_json_buf, sizeof(dust_json_buf),
-            "\"altitude_m\":%d,"
-			"\"sensor_value\":%u"
-            "}", 
-            50, pm2_5);
-	ring_buffer_queue_arr(rb, dust_json_buf, strlen(dust_json_buf));
-}
-
-void appendLeftBrace(ring_buffer_t* rb)
-{
-	ring_buffer_queue(rb, '{');	
-}
-
-void appendRightBrace(ring_buffer_t* rb)
-{
-	ring_buffer_queue(rb, '}');	
 }
--- sys/json.h
+++ sys/json.h
@@ -14,45 +14,15 @@
  */
 void getJsonData(ring_buffer_t* rb, char* buf); 
 
-/**
- * @brief   Parse and store dust data to JSON ring buffer
- * @param   rb is ring buffer address to put data in 
- * @param   lat is latitude
- * @param   lon is longitude
- * @param   pm2_5 is PM2.5 value
+ /**
+ * @brief   Format data to JSON string and store into ring buffer
+ * @param   rb Address of ring buffer to store the JSON string
+ * @param   key Site identifier string (e.g., "us" or "hs")
+ * @param   row Calculated row index
+ * @param   col Calculated column index
+ * @param   pm2_5 PM2.5 sensor value
  * @return  none
  */
-void parseAllDataToJson(ring_buffer_t* rb, double lat, double lon, uint16_t pm2_5);
-
-/**
- * @brief   Parse and store dust data to JSON ring buffer
- * @param   rb is ring buffer address to put data in 
- * @param   pm2_5 is PM2.5 value
- * @return  none
- */
-void parseDustDataToJson(ring_buffer_t* rb, uint16_t pm2_5);
-
-/**
- * @brief   Parse and store gps data to JSON ring buffer
- * @param   rb is ring buffer address to put data in
- * @param   lat is latitude
- * @param   lon is longitude
- * @return  none
- */
-void parseGpsDataToJson(ring_buffer_t* rb, double lat, double lon);
-
-/**
- * @brief   add left brace to JSON ring buffer
- * @param   rb is ring buffer address to put data in 
- * @return  none
- */
-void appendLeftBrace(ring_buffer_t* rb);
-
-/**
- * @brief   add right brace to JSON ring buffer
- * @param   rb is ring buffer address to put data in 
- * @return  none
- */
-void appendRightBrace(ring_buffer_t* rb);
+void parseAllDataToJson(ring_buffer_t* rb, char* key, int row, int col, uint16_t pm2_5);
 
 #endif
